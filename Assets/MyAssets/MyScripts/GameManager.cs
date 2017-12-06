2c9d93bc0417318324d09e6f2398b2b19c6993b5
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-12-05 19:46:44 -0500

    bird spawn location and game manager

---
+++ Assets/MyAssets/MyScripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject birdPrefab;
+	public Transform birdSpawnLocation;
+
+	// Use this for initialization
+	void Start () {
+		SpawnBird ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnBird() {
+		Instantiate (birdPrefab, birdSpawnLocation);
+	}
+}

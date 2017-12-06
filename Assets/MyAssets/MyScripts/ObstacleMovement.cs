573b0a9e257d944adf343814d40762beda725eb8
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-12-05 20:53:10 -0500

    obstacles move to the left

---
+++ Assets/MyAssets/MyScripts/ObstacleMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);
+
+		if (transform.position.x < -15) {
+			Destroy (gameObject);
+		}
+		
+	}
+}

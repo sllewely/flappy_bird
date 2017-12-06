1e6f30bd3bbb37ee311fea62090ae30b97c16082
Sarah Llewelyn <sllewelyn@renttherunway.com>
2017-12-05 19:13:23 -0500

    bird force

---
+++ Assets/MyAssets/MyScripts/BirdController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+	public Rigidbody2D myBirdsRigid;
+	public float upForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myBirdsRigid.AddForce (Vector2.up * upForce);
+		}
+		
+	}
+}
